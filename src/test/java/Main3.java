5ea7de0e29fb58a973bc58832b564a9143f821b4
IrinaYu <irinayurchenko27@gmail.com>
2020-09-24 21:51:48 +0300

    Yaml for Jenfins

---
+++ src/test/java/Main3.java
@@ -0,0 +1,20 @@
+import org.yaml.snakeyaml.Yaml;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Map;
+
+public class Main3 {
+    public static void main(String[] args) {
+        FileInputStream file = null;
+        try {
+            file = new FileInputStream(new File("src/main/resources/config.yaml"));
+            Yaml yaml = new Yaml();
+            Map<String, Object> result = (Map<String, Object>) yaml.load(file);
+            System.out.println(result.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
